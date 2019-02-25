5a35c1cba5178356ac243b474707ee6b5ad0617e
Samuel <sumlebulle@hotmail.com>
2019-02-25 14:05:50 +0100

    Created class playhangman wip

---
+++ hangman/hangman/PlayHangman.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hangman
+{
+    public class PlayHangman
+    {
+        public Word PickedWord { get; set; }
+
+        private List<string> RightLetters;
+        private List<string> GuessedLetters;
+        private List<string> WrongLetters;
+
+        public PlayHangman()
+        {
+            RightLetters = new List<string>();
+            GuessedLetters = new List<string>();
+            WrongLetters = new List<string>();
+        }
+
+        public void Play()
+        {
+            RightLetters = new List<string>();
+
+            for (int i = 0; i < PickedWord.WordLength; i++)
+            {
+                RightLetters.Add(" _ ");
+            }
+
+            for (int i = 0; i < PickedWord.WordLength; i++)
+            {
+                string letter = PickedWord.Content.Substring(i,1);
+                if (GuessedLetters.Count > 0)
+                {
+                    foreach (string guessedLetter in this.GuessedLetters)
+                    {
+                        if (letter.Equals(guessedLetter.Trim().ToUpper()))
+                        {
+                            RightLetters.RemoveAt(i);
+                            RightLetters.Insert(i, " {$letter} ");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
